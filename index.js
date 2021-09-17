efafbe4325aa22f1185dddb90b59a104a457be2b
max99xam <pepetrov123@list.ru>
2021-09-17 20:07:58 +0300

    first commit

--- index.js
+++ index.js
@@ -1,20 +1,10 @@
-//const addon = require("bindings")("FDTD.node");
 const addon = require("./build/Release/napi-addon-fdtd.node");
 
-// console.log("exports: ", addon);
-// exports.addonFDTD = addon;
 module.exports = addon;
 
-  // addon.getFDTD_2D([1, 10, 1], false);
-  // addon.getFDTD_2D([1, 10, 1], false);
-  // addon.getFDTD_2D([1, 10, 1], false);
-  //  console.log("data1: ", addon.getFDTD_2D([1, 10, 1], false).currentTick);
-  // console.log("data1: ", addon.getFDTD_2D([1, 10, 1], false));
-  // console.log("data1: ", addon.getFDTD_2D([1, 10, 1], true));
+// console.log("data1: ", addon.getFDTD_2D([1, 10, 1], false));
+// console.log("data1: ", addon.getFDTD_2D([1, 10, 1], true));
 
-//  console.log("data2: ", addon.getFDTD_2D([1, 11, 1]));
-
-//  console.log("test: ");
 //  console.log("data1: ", addon.getFDTD_3D([1, 3, 1], false));
 //  console.log("data2: ", addon.getFDTD_3D([1, 3, 1], false));
 //  console.log("data3: ", addon.getFDTD_3D([1, 3, 1], false));
